96ce09248ba5924e66bff32079fde5ee8dc53537
Maxime Moreillon <m.moreillon@gmail.com>
2026-07-24 16:42:08 +0900

    CRD

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from db import SessionDep, Fruit, create_db_and_tables
 from sqlmodel import select
 from typing import Annotated
-import jinja_partials
 
 app = FastAPI()
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-jinja_partials.register_fastapi_extensions(app, templates)
 
 
 @app.get("/", response_class=RedirectResponse)
@@ -20,14 +17,16 @@ async def index():
     return RedirectResponse(url="/fruits")
 
 
-@app.post("/fruits")
+@app.post("/fruits", response_class=HTMLResponse)
 async def create_fruit(
     fruit: Annotated[Fruit, Form()], session: SessionDep, request: Request
 ):
     session.add(fruit)
     session.commit()
     session.refresh(fruit)
-    return templates.TemplateResponse(request=request, name="fruits/new/success.html")
+    return templates.TemplateResponse(
+        request=request, name="partials/fruit.html", context={"fruit": fruit}
+    )
 
 
 @app.get("/fruits", response_class=HTMLResponse)
@@ -48,6 +47,12 @@ async def read_fruit(request: Request, id: str):
     return templates.TemplateResponse(
         request=request, name="item.html", context={"id": id}
     )
+
+
+@app.delete("/fruits/{id}", response_class=HTMLResponse)
+async def delete_fruit(request: Request, id: str):
+    # TODO: actually delete
+    return ""
 
 
 @app.on_event("startup")
