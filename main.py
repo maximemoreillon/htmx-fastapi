91a34034fa74d09975309229bf6af62b01913af2
Maxime Moreillon <m.moreillon@gmail.com>
2026-07-24 16:21:02 +0900

    layout

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from db import SessionDep, Fruit
+from db import SessionDep, Fruit, create_db_and_tables
 from sqlmodel import select
 from typing import Annotated
+import jinja_partials
 
 app = FastAPI()
 
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+jinja_partials.register_fastapi_extensions(app, templates)
 
 
 @app.get("/", response_class=RedirectResponse)
@@ -47,3 +48,8 @@ async def read_fruit(request: Request, id: str):
     return templates.TemplateResponse(
         request=request, name="item.html", context={"id": id}
     )
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
